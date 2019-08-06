7e8476a437b7e04bdccb27c51c2ea1a560e7dd36
Hanna <hannaaikas@gmail.com>
2019-08-06 08:46:04 +0100

    use views to separate routing concerns and presentation concerns

--- app.rb
+++ app.rb
@@ -1,18 +1,7 @@
 require 'sinatra'
-require 'shotgun'
-
-get '/' do
-  'hello!'
-end
-
-get '/secret' do
-  'I have a NEW EXCITING secret message'
-end
 
 get '/cat' do
-  "<div style='border: 3px dashed red'>
-     <img src='http://bit.ly/1eze8aE'>
-   </div>"
+  erb(:index)
 end
 
 set :session_secret, 'super secret'
